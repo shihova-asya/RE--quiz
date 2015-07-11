f4ee63252f79a746079d8f33bc89fee5446a25eb
shihova-asya <shihova.asya@mail.ru>
2015-07-11 14:26:08 +0400

    Better art style

--- test_start.php
+++ test_start.php
@@ -2,39 +2,47 @@
 <head>
   <title>Старт</title>
   <meta charset="utf-8">
+  <style type="text/css">
+   h1 { 
+    font-size: 120%; 
+    font-family: Verdana, Arial, Helvetica, sans-serif; 
+    color: #CD5555;
+   }
+   b { 
+    font-size: 107%; 
+   }
+  </style>
 </head>
-<body bgcolor="#87CEFF">
-<font face="sans-serif">
-  <b>Правила игры просты - ответьте на пять вопросов, касающихся разных статей бюджета Российской Империи (только расходы) в 
-    1913 году.</b><br><br>
+<body bgcolor="#FFDEAD">
+  <h1>Правила игры просты - ответьте на пять вопросов, касающихся разных статей бюджета Российской Империи (только расходы) в 
+    1913 году.</h1>
   <form name="questions" method="get" action="result.php">
-  Сколько рублей получила Российская Империя в 1912 году на акцизах с табака?<br> 
+  <b>Сколько рублей получила Российская Империя в 1912 году на акцизах с табака?</b><br> 
     <input type="radio" name="first_answer" value="false"> 5700000<br>
     <input type="radio" name="first_answer" value="true"> 61000000<br>
     <input type="radio" name="first_answer" value="false"> 130000<br>
     <input type="radio" name="first_answer" value="false"> 440000000<br>
-  Каковы были подымные подати и сборы?<br> 
+  <b>Каковы были подымные подати и сборы?</b><br> 
     <input type="radio" name="second_answer" value="false"> 5820322<br>
     <input type="radio" name="second_answer" value="false"> 3850222<br>
     <input type="radio" name="second_answer" value="false"> 852322<br>
     <input type="radio" name="second_answer" value="true"> 2850322<br>
-  А каков был сахарный доход?<br> 
+  <b>А каков был сахарный доход?</b><br> 
     <input type="radio" name="third_answer" value="true"> 128430000<br>
     <input type="radio" name="third_answer" value="false"> 12843000<br>
     <input type="radio" name="third_answer" value="false"> 1128430000<br>
     <input type="radio" name="third_answer" value="false"> 0<br>
-  Суммарный доход от всех пошлин?<br> 
+  <b>Суммарный доход от всех пошлин?</b><br> 
     <input type="radio" name="forth_answer" value="false"> 11847376<br>
     <input type="radio" name="forth_answer" value="true"> 191847376<br>
     <input type="radio" name="forth_answer" value="false"> 1991847376<br>
     <input type="radio" name="forth_answer" value="false"> 19991847376<br>
-  А чем  может похвастаться Главное Интендантское Управление?<br> 
+  <b>А чем  может похвастаться Главное Интендантское Управление?</b><br> 
     <input type="radio" name="fifth_answer" value="false"> 17500<br>
     <input type="radio" name="fifth_answer" value="false"> 137500<br>
     <input type="radio" name="fifth_answer" value="true"> 1337500<br>
     <input type="radio" name="fifth_answer" value="false"> 13337500<br>
-    <input type="submit" value="Подтвердите свой выбор">
+    <input type="submit" value="Подтвердите свой выбор" style="font-size: 107%; color: #CD5555">
   </form>
-</font>
 </body>
 </html>
