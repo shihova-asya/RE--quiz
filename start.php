61514f76e3a535c9f9215eb5a480d37077c95a7a
shihova-asya <shihova.asya@mail.ru>
2015-07-19 19:12:30 +0400

    5 chhozed questions are different, plus image

--- start.php
+++ start.php
@@ -1,16 +1,27 @@
-<html>
+﻿<html>
 <head>
   <title>Старт</title>
   <meta charset="utf-8">
   <style type="text/css">
-   h1 { 
-    font-size: 120%; 
-    font-family: Verdana, Arial, Helvetica, sans-serif; 
-    color: #CD5555;
-   }
-   b { 
-    font-size: 107%; 
-   }
+    #questions {
+      position: absolute;
+      top: 10px;
+      left: 20px;
+      font-size: 150%;
+    }
+    #my_timer {
+      position: absolute;
+      bottom: 20px;
+      right: 20px;
+    }
+    h1 { 
+      font-size: 120%; 
+      font-family: Verdana, Arial, Helvetica, sans-serif; 
+      color: #CD5555;
+    }
+    b { 
+      font-size: 107%; 
+    }
   </style>
   <script type="text/javascript">
   var used_time = 0;
@@ -45,7 +56,7 @@
 </head>
 <body bgcolor="#FFDEAD" onload="startTimer()">
   <span id="my_timer" style="color: #8B0000; font-size: 150%;">00:00:00</span><br>
-<?php
+<?php 
   $fp = fopen('43.csv', 'r');
   // У меня два массива - первый (text) сопоставляет id вопроса и текст, второй (answer) - id и число.
   $text = array(); $answer = array();
@@ -55,36 +66,46 @@
     $str = fgets($fp, 999);
     $number = substr($str, strripos($str, ",") + 1);
     $str = substr($str, 0, strripos($str, ","));
-      for ($idx = 0; $idx < 3; $idx++) {
+      for ($index = 0; $index < 3; $index++) {
         $str = substr($str, stripos($str, ",") + 1);
       }
     $str = trim($str, '[".]');
     if ($number > 0 and !in_array($str, $text)) { // Берём в качестве вопроса
       $id++;      
       $text[$id] = $str;
-      $answer[$id] = $number; // $id - число вопросов, около 5000
+      $answer[$id] = $number; // $id - число вопросов
     }
   }  
   fclose($fp);
-  $count = $_GET['count'];
-  if ($count == '' )
-    $count = 0;
   $idx = $_GET['idx'];
-  if ($idx == '') {
-    $idx = 0;
+  $count = $_GET['count'];
+  if ($idx == '') { // Первый вопрос
+    $idx = 0; $count = 0; $all_used_time = 0;
+    $fp = fopen('data.txt', 'w');
+    $five = array(); // $five - массив из пяти произвольно выбранных вопросов.
+    while (count($five) < 5) {
+      $num = rand(1, $id);
+      if (!in_array($num, $five)) {
+        $five[] = $num;
+        fwrite($fp, $num.' 
+');
+      }
+    }
+    fclose($fp);
   } else {
     $idx++;
-    }
-  if ($_GET['all_used_time'] == '') {
-    $all_used_time = 0;
-  } else {
     $all_used_time = $_GET['used_time'] + $_GET['all_used_time'];
-    }
+    }   
   if ($_GET['answer'] == 'true')
     $count++;
-  $quest = rand(1, $id);
+  $fp = fopen('data.txt', 'r');
+  for ($index = 0; $index <= $idx; $index++) {
+    $quest = fgets($fp, 999);
+  }
+  $quest = 0 + $quest;
+  fclose($fp);
   if ($idx <= 4) {
-    echo '<form id="questions" method="get" action="start.php">'; 
+    echo '<form id="questions" method="get">'; 
     echo "<b>".$text[$quest]." </b><br> \n"; // Название вопроса
     $true_ans = rand(1, 4);
     for ($idx2 = 1; $idx2 < 5; $idx2++) {
@@ -93,19 +114,17 @@
         $value_of_truth = "true";
         $value_of_number = $answer[$quest];
       } else {
-        $value_of_number = rand(1, $id);
+        $value_of_number = $answer[rand(1, $id)];
         }
       echo '<input type="radio" onclick=Send_time("'.$value_of_truth.'")>'.$value_of_number.' <br>';
     }
     echo '<input type="hidden" name="count" value="'.$count.'">';
     echo '<input type="hidden" name="idx" value="'.$idx.'">';
     echo '<input type="hidden" name="all_used_time" value="'.$all_used_time.'">';
-    echo '<input type="submit" style="font-size: 107%; color: #CD5555" value="Подтвердите свой выбор"></form>'; 
+    echo '<input type="submit" style="font-size: 107%; color: #CD5555" value="Подтвердите свой выбор"> </form>'; 
   } else {
-    echo '<form name="questions" method="get" action="result.php">';
-    echo '<input type="hidden" name="count" value="'.$count.'">';
-    echo '<input type="hidden" name="all_used_time" value="'.$all_used_time.'">';
-    echo '<input type="submit" style="font-size: 107%; color: #CD5555" value="Посмотреть результаты "></form>';
+    echo '<h1>Кликните на картинку, чтобы посмотреть результаты<h1>';
+    echo '<a href="result.php?count='.$count.'&all_used_time='.$all_used_time.'" ><img src="start.jpg" width="70%" height="80%"></a>';
     }
 ?>
 </body>
